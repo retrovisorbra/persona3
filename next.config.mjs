213ce87a5b6a0dac3ffd67f842856d98b17ddcb6
retrovisor <andrenfaust@gmail.com>
2024-08-16 12:37:44 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const nextConfig = {
     return [
       {
         source: '/ingest/static/:path*',
-        destination: 'https://us-assets.i.posthog.com/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
       },
       {
         source: '/ingest/:path*',
-        destination: 'https://us.i.posthog.com/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
       },
     ]
   },
